8e18494bc155d5c89b07d37040df7fee35b35b86
appj0 <noapertr@gmail.com>
2026-02-16 00:36:35 +1100

    Add Supabase public config for global leaderboard

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.GLOBAL_DATA_CONFIG = {
   // Example: "https://YOUR_PROJECT_ID.supabase.co"
   supabaseUrl: "https://gzixmlkymxzrwtfoyztg.supabase.co",
   // Supabase anon/public key.
-  supabaseAnonKey: "",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6aXhtbGt5bXh6cnd0Zm95enRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExNDE1OTEsImV4cCI6MjA4NjcxNzU5MX0.lbSgtmO7hIf6rYtCmojvGE_lorUaJR0uAiOgoMzwKzQ",
   // Table name that stores leaderboard rows.
   table: "leaderboard_runs"
 };
